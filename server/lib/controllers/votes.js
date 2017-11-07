a87badca40c9d075c2e5c3a65413e519b7dcca08
idrees <idreesibraheem@gmail.com>
2017-11-07 20:55:12 +0100

    ft(downvote-recipe): Added feature to enable users downvote a recipe

--- server/lib/controllers/votes.js
+++ server/lib/controllers/votes.js
@@ -1,12 +1,61 @@
 import { Votes, Recipes } from '../model';
 
 
-const voted = false;
+
 /*
   A Votes class that allows a user to upvote or downvote a recipe
  */
 export default class Vote {
   static upVotes(req, res) {
+    const id = req.params.recipeid;
+    const userId = req.decoded.id;
+    let voted = false;
+    let newVotes;
+    if (!userId) {
+      return res.status(401).send({
+        success: false,
+        message: 'You are not authorized to do this.'
+      });
+    }
+    return Recipes
+      .find({
+        where: {
+          userId,
+          id
+        }
+      })
+      .then((recipe) => {
+        if (!recipe) {
+          return res.status(404).send({
+            message: 'Recipe not found',
+            data: recipe,
+          });
+        }
+        recipe.increment('upVotes', { by: 1 });
+        voted = true;
+        return Votes // need re-migrate table again in order to test this elaborately
+          .create({
+            userId,
+            recipeId: id,
+            voted,
+          })
+          .then((vote) => {
+            return res.status(201).send({
+              status: 'Success',
+              message: `${recipe.title} has been upvoted`,
+              data: {
+                recipe,
+                vote
+              }
+            });
+            // console.log('the votes', vote);
+          })
+          .catch(err => res.status(400).send(err));
+      })
+      .catch(err => res.status(400).send(err));
+  }
+
+  static downVotes(req, res) {
     const id = req.params.recipeid;
     const userId = req.decoded.id;
     if (!userId) {
@@ -28,15 +77,15 @@ export default class Vote {
             message: 'Recipe not found',
             data: recipe,
           });
-        }        
-        recipe.increment('upVotes', { by: 1 });
+        }
+        // recipe.decrement('upVotes', { by: 1 });
+        recipe.decrement('downVotes', { by: 1 });
         return res.status(201).send({
           status: 'Success',
-          message: `${recipe.title} has been upvoted`,
+          message: `${recipe.title} has been downvoted`,
           data: recipe,
         });
       })
       .catch(err => res.status(400).send(err));
-      
   }
 }
